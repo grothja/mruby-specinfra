7a02c625c41ca1f9147715b4befb36ec202872dc
Jonathan Groth <grothja@niaid.nih.gov>
2019-07-14 02:05:03 -0400

    add support for Clear Linux

---
+++ mrblib/specinfra/helper/detect_os/clearlinux.rb
@@ -0,0 +1,24 @@
+module Specinfra
+  module Helper
+    class DetectOs
+      class Clearlinux < Specinfra::Helper::DetectOs
+        def detect
+          if run_command('swupd').success?
+            distro  = nil
+            release = nil
+            os_release = run_command("cat /etc/os-release")
+            if os_release.success?
+              os_release.stdout.each_line do |line|
+                distro  = line.split('=').last.strip if line =~ /^NAME=/
+                release = line.split('=').last.strip if line =~ /^VERSION_ID=/
+              end
+            end
+            distro ||= 'clearlinux'
+            release ||= nil
+            { family: distro.gsub(/[^[:alnum:]]/, '').downcase, release: release }
+          end
+        end
+      end
+    end
+  end
+end
